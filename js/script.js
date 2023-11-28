a237396f8802d7dc3f06d94866c52c853accb3a0
TheVaibhavTemple <vyadav1910@gmail.com>
2023-11-28 16:29:21 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,145 @@
+
+
+    var calendarEl = document.getElementById('calendar');
+
+    var resource = [
+        {
+            id: 'a',
+            title: 'Doctor A'
+          },
+          {
+            id: 'b',
+            title: 'Doctor B'
+          },
+          {
+            id: 'c',
+            title: 'Doctor C'
+          }
+      ];
+
+
+      var events =  [
+        {
+            id: 'a',
+            title: 'Mr. Vaibhav Yadav',
+            start: "2023-11-28T02:00:00",
+            end: "2023-11-28T05:00:00",
+            resourceId: 'a',
+        }
+      ];
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'resourceTimeGridDay',
+      resources:  resource,
+      events : events,
+      editable: true,
+      eventOverlap: false,
+      nowIndicator: true
+    });
+
+
+
+
+    calendar.render();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var resources = [
+//   {
+//     id: 'a',
+//     title: 'Room A'
+//   },
+//   {
+//     id: 'b',
+//     title: 'Room B'
+//   },
+//   {
+//     id: 'c',
+//     title: 'Room C'
+//   },
+//   {
+//     id: 'd',
+//     title: 'Room D'
+//   },
+//   {
+//     id: 'e',
+//     title: 'Room E'
+//   }
+// ]; 
+ 
+ 
+
+
+// var events = [
+//   {
+//     id: 'a',
+//     title: 'my event',
+//     start: '2023-11-23T10:30:00',
+//     end: '2023-11-23T11:30:00',
+//     resourceId: 'a'
+//   },
+
+//   {
+//     id: 'b',
+//     title: 'my event 2',
+//     start: '2023-11-22T13:30:00',
+//     end: '2023-11-22T14:30:00',
+//     resourceId: 'b'
+//   },
+
+//   {
+//     id: 'c',
+//     title: 'my event',
+//     start: '2018-09-01'
+//   }
+// ]
+ 
+ 
+ 
+//  var calendarEl = document.getElementById('calendar');
+//   var calendar = new FullCalendar.Calendar(calendarEl, {
+//     initialView: 'resourceTimeGridDay',
+//       resources: resources,
+//       events : events,
+//       editable: true,
+//       eventOverlap: false,
+//       eventResizeStart: function(info){
+//         console.log(info);
+//       }
+//     }
+// );
+//   calendar.render();
